4c283045c9744392d9f051a2524b3b623b16b571
JennerDest169 <cabelloalvaradojenner@gmail.com>
2025-08-15 18:26:05 -0500

    googleBack

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,6 +2,12 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
   database: drizzleAdapter(
     {},
     {
